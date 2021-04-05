05421454fff0ea9647b3a1a2ed2afba01c6be733
Junya Morioka <mjun@mjunya.com>
2021-04-05 16:58:26 +0900

    update

---
+++ script/replace_name.sh
@@ -0,0 +1,15 @@
+echo "Input old text"
+read oldText
+
+echo "Input new text"
+read newText
+
+
+#フォルダ名、ファイル名を置換する
+find . -maxdepth 1 -name '*'$oldText'*' | \
+while read line
+do newline=$(echo $line | sed 's/'$oldText'/'$newText'/g')
+    echo $newline
+    mv "$line" $newline
+done
+
