68f79406f2c818d325983ada9552f371d3e33ae5
Junya Morioka <mjun@mjunya.com>
2021-07-26 17:12:32 +0900

    update

---
+++ bin/apt.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install -y \
+    make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev \
+    libgl1-mesa-dev \
+    # End Python build dependencies
+    git \
+    zsh 
