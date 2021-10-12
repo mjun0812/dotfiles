cdfa8f7c8c0aebef98bd8d094e05841c302ef163
Junya Morioka <mjun@mjunya.com>
2021-10-12 09:30:41 +0900

    fix

--- bin/neovim.sh
+++ bin/neovim.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+CURRENT=$(dirname $0)
+
+source ${CURRENT}/get_os_info.sh
+
 # is_exists returns true if executable $1 exists in $PATH
 is_exists() {
     which "$1" >/dev/null 2>&1
@@ -8,10 +12,16 @@ is_exists() {
 
 install_neovim() {
     if ! is_exists "nvim -v"; then
-        mkdir -p ~/.bin
-        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-        chmod u+x ./nvim.appimage
-        mv ./nvim.appimage ~/.bin/nvim
+        case `get_os_name` in
+            macOS)
+                brew install neovim
+                ;;
+            *)
+                mkdir -p ~/.bin
+                curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+                chmod u+x ./nvim.appimage
+                mv ./nvim.appimage ~/.bin/nvim
+            esac
     fi
 }
 
