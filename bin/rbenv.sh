3275ebfdc2de1b014d8f2e9f5c8592bdbb8920dc
Junya Morioka <mjun@mjunya.com>
2022-05-30 15:23:18 +0900

    update

--- bin/rbenv.sh
+++ bin/rbenv.sh
@@ -8,6 +8,10 @@ is_exists() {
 
 install_rbenv() {
     if [ -d ~/.rbenv ]; then
+        CURRENT=`pwd`
+        cd ~/.rbenv && git pull
+        cd ~/.rbenv/plugins/ruby-build && git pull
+        cd $CURRENT
     else
         git clone https://github.com/rbenv/rbenv.git ~/.rbenv
         git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
