5c4a0d14341f8c6bd1850d82614b754eafbefe33
Junya Morioka <mjun@mjunya.com>
2023-01-01 14:43:38 +0900

    update

--- bin/dein.vim.sh
+++ bin/dein.vim.sh
@@ -2,9 +2,7 @@
 
 install_dein() {
     mkdir -p ~/.cache
-    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >installer.sh
-    sh ./installer.sh ~/.cache/dein
-    rm -f ./installer.sh
+    sh -c "$(wget -O- https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
 }
 
 install_dein
