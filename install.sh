41e3a263ddce43e92d1c2cfcba5b1db9370e05fb
Junya Morioka <mjun@mjunya.com>
2023-05-06 15:55:17 +0900

    update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ GITHUB='https://github.com/mjun0812/dotfiles.git'
 
 PYTHON_VERSION='3.10.6'
 RUBY_VERSION='3.0.4'
-NODE_VERSION='16.17.0'
+NODE_VERSION='18.12.1'
 
 # is_exists returns true if executable $1 exists in $PATH
 is_exists() {
