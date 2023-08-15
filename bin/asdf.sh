fcd8e96e7fce4553d8f71f592def1387bca11bfc
Junya Morioka <mjun@mjunya.com>
2023-08-16 02:38:55 +0900

    update

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -4,6 +4,10 @@ if [ -d ~/.asdf ]; then
     asdf update
 else
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    . "$HOME/.asdf/asdf.sh"
+    asdf plugin add golang
+    asdf plugin add ruby
+    asdf plugin add nodejs
     source ~/.zshrc
     asdf install
 fi
