c9632b36dc3e94819c6eb20287f29ac464ca13c8
Junya Morioka <mjun@mjunya.com>
2023-08-16 16:26:19 +0900

    update

--- bin/pyenv.sh
+++ bin/pyenv.sh
@@ -1,22 +1,13 @@
 #!/bin/zsh
 
-# is_exists returns true if executable $1 exists in $PATH
-is_exists() {
-    which "$1" >/dev/null 2>&1
-    return $?
-}
+if [ -d ~/.pyenv ]; then
+    pyenv update
+else
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    source ~/.zshrc
+    git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+fi
 
-install_pyenv() {
-    if [ -d ~/.pyenv ]; then
-        pyenv update
-    else
-        git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-        source ~/.zshrc
-        git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
-        git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-    fi
-}
-
-install_pyenv
 pyenv install --skip-existing "$1"
 pyenv global "$1"
