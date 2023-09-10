45e556785f5472f74e32a9328765173085689287
Junya Morioka <mjun@mjunya.com>
2023-09-11 01:08:58 +0900

    migrate vimplug from dein

--- bin/dein.vim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-install_dein() {
-    mkdir -p ~/.cache
-    sh -c "$(wget -O- https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
-}
-
-install_dein
-source ~/.zshrc
