45559ada43ba7eeec3d8bf8727af7d251337ceb2
Junya Morioka <mjun@mjunya.com>
2025-10-13 00:44:49 +0900

    update

---
+++ config/dot/opt.zsh
@@ -0,0 +1,32 @@
+# Completion
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt ALWAYS_TO_END        # Move cursor to the end of a completed word.
+setopt PATH_DIRS            # Perform path search even on command names with slashes.
+setopt AUTO_MENU            # Show completion menu on a successive tab press.
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+setopt AUTO_PARAM_SLASH     # If completed parameter is a directory, add a trailing slash.
+setopt EXTENDED_GLOB        # Needed for file modification glob modifiers with compinit.
+setopt MENU_COMPLETE        # autoselect the first completion entry.
+unsetopt FLOW_CONTROL       # Disable start/stop characters in shell editor.
+
+# Directory stack
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt MULTIOS              # Write to multiple descriptors.
+
+# Command correction
+setopt CORRECT              # Correct the spelling of a command before executing it.
+setopt COMBINING_CHARS      # Combine zero-length punctuation characters (accents)
+                            # with the base character.
+setopt INTERACTIVE_COMMENTS # Enable comments in interactive shell.
+setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry's Garage'.
+
+# Jobs
+setopt LONG_LIST_JOBS       # List jobs in the long format by default.
+setopt NOTIFY               # Report status of background jobs immediately.
+unsetopt BG_NICE            # Don't run all background jobs at a lower priority.
+unsetopt HUP                # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS         # Don't report on jobs when shell exit.
